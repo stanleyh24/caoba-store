415cec8a8e78672d711b7b11f97fc669197cf022
Stanley Hidalgo <stanleyhidalgo@gmail.com>
2022-12-27 10:38:39 -0400

    file resturcture

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from "react-router-dom"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
